419e3bce2b77ca63212e4f22f8d2ac0324db0aa9
Vibhav Patel <77201529+Vibhav1207@users.noreply.github.com>
2025-08-19 14:33:49 +0530

    Updated

---
+++ 19-8-25/Question2.java
@@ -0,0 +1,26 @@
+// A person is eligible to vote if their age is 18 or above. If they are 18 or above, check if they have a voter ID card. If they do, print "You are eligible to vote." If they don't have a voter ID card, print "You need a voter ID to vote."
+
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+
+        int age = sc.nextInt();
+        sc.nextLine(); 
+        System.out.println("Do you have a voter ID card? (yes/no)");
+        String hasVoterID = sc.nextLine();
+
+        if (age >= 18) {
+            if (hasVoterID.equalsIgnoreCase("yes")) {
+                System.out.println("You are eligible to vote.");
+            } else {
+                System.out.println("You need a voter ID to vote.");
+            }
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+        sc.close();
+    }
+}
