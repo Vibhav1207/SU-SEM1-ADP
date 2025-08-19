7c28e52c73c423757fafe7b16c1d8d05254fdd87
Vibhav Patel <77201529+Vibhav1207@users.noreply.github.com>
2025-08-19 13:54:17 +0530

    Updated todays code

---
+++ 19-8-25/ifelse.java
@@ -0,0 +1,14 @@
+public class ifelse {
+    public static void main(String[] args){
+        int a = 5,b=7;
+
+        if(a>b){
+            System.out.println("a is greater than b");
+            
+        }
+        else{
+            System.out.println("b is greater than a");
+        }
+
+    }
+}
---
+++ 19-8-25/question1.java
@@ -0,0 +1,20 @@
+// WAP to check if a number is positive, negative, or zero
+
+import java.util.Scanner;
+
+public class question1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int num = sc.nextInt();
+
+		if (num > 0) {
+			System.out.println(num + " is positive.");
+		} else if (num < 0) {
+			System.out.println(num + " is negative.");
+		} else {
+			System.out.println("The number is zero.");
+		}
+		sc.close();
+	}
+}
