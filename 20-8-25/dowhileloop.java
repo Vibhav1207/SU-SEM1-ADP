f821f57f290e3562dd170175b0cb6b8f7eee76a1
Vibhav Patel <77201529+Vibhav1207@users.noreply.github.com>
2025-08-20 10:49:56 +0530

    Add do-while loop example and update while loop range
    Added a new Java example demonstrating a do-while loop that prints numbers from 1 to 50. Updated the existing while loop example to print numbers from 1 to 80 instead of 1 to 50.

---
+++ 20-8-25/dowhileloop.java
@@ -0,0 +1,11 @@
+//WAP for do-while loop 
+
+public class dowhileloop {
+    public static void main(String[] args){
+        int a =1;
+        do {
+            System.out.println(a);
+            a++;
+        } while (a <= 50);
+    }
+}
--- 20-8-25/whileloop.java
+++ 20-8-25/whileloop.java
@@ -3,7 +3,7 @@
 public class whileloop {
     public static void main(String[] args){
         int i = 1;
-        while (i <= 50) {
+        while (i <= 80) {
             System.out.println(i);
             i++;
         }
