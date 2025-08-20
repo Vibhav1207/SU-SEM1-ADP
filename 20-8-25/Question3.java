64e6611d362df9debafe57149e72fb49cf2aa72c
Vibhav Patel <77201529+Vibhav1207@users.noreply.github.com>
2025-08-20 11:38:23 +0530

    Add factorial program and update README
    Added Question3.java to calculate the factorial of a user-input number. Updated README.md to include the new program in the list.

---
+++ 20-8-25/Question3.java
@@ -0,0 +1,16 @@
+//WAP to get factorial of a user-input number
+import java.util.Scanner;
+
+public class Question3{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+         long factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+
+        }
+        System.out.println("The factorial of " + num + " is: " + factorial);
+    }
+}
