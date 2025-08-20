f009110e2dc76e3111ecc0700bb47924471a3767
Vibhav Patel <77201529+Vibhav1207@users.noreply.github.com>
2025-08-20 10:53:44 +0530

    Add program to print numbers from 50 to 1
    Added Question1.java and its compiled class file, which prints numbers from 50 to 1 in reverse order using a while loop. Updated whileloop.class, possibly due to related changes or recompilation.

---
+++ 20-8-25/Question1.java
@@ -0,0 +1,13 @@
+//WAP to print number form 50 to 1 in reverse order
+
+public class Question1{
+    public static void main(String[] args)
+  {
+    int A=50;
+    while (A >= 1){
+        System.out.println(A);
+        A --;
+    }
+
+    }
+}
